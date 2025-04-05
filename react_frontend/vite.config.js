8f0f19d7c8b63487f41a5dfc3f3fc5500389f5b3
Rahel Teferi <rahel.tem@gmail.com>
2025-04-05 11:17:47 +0200

    change vite.config.js

--- react_frontend/vite.config.js
+++ react_frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 100,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
   plugins: [react()],
-})
+});
