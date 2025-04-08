92f49b44ea8bab7b11d7ee9df75d88d95324045d
Rahel Teferi <rahel.tem@gmail.com>
2025-04-08 08:26:07 +0200

    fixing typo

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.get("/books/:id", BooksControllers.getBooksInfo);
 app.put("/books/:id", BooksControllers.updateBook);
 app.delete("/books/:id", BooksControllers.deleteBook);
 app.post("/books", BooksControllers.addBooks);
-app.get("/catagories", BooksControllers.catagorizeBooks);
+app.get("/catagories", BooksControllers.categorizeBooks);
 
 app.get("/users", UsersControllers.getUsers);
 app.get("/loans/users", UsersControllers.getUsersToLoan);
